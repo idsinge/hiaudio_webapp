f8d6e1b57ffcf466f7a864b808c1fb0b87a9d821
fan-droide <jose.gilpanal@telecom-paris.fr>
2023-07-18 17:06:02 +0200

    dialog for new collection

--- src/index/index.js
+++ src/index/index.js
@@ -18,11 +18,14 @@ if (domainIs !== 'localhost:80' && window.location.origin !== 'http://localhost'
 
 if (isAuthenticated) {
   document.getElementById('useroptions').innerHTML = `<li class="nav-item">
-    <a class="nav-link" href="${uriProfilePage+'/profile.html'}">Profile</a>
+    <a class="nav-link" href="${uriProfilePage + '/profile.html'}">Profile</a>
   </li>
   <li class="nav-item">
-        <a class="nav-link" href="#" data-toggle="modal" data-target="#newMusicModal">New Music</a>
-      </li>`
+        <a class="nav-link" href="#" data-toggle="modal" data-target="#newMusicModal">/ New Music</a>
+  </li>
+  <li class="nav-item">
+      <a class="nav-link" href="#" id="createNewCollButton" data-toggle="modal" data-target="#newCollectionModal">/ New Collection</a>
+  </li>`
 } else {
   document.getElementById('useroptions').innerHTML = `<a class="dropdown-item" href="${window.location.origin}/login">Google Login</a>`
 }
@@ -88,43 +91,3 @@ const paintListOfCompositions = (compositionsList) => {
   document.getElementById('grid').insertAdjacentHTML('afterbegin', listElelemts)
   document.getElementById('searchInput').removeAttribute('disabled')
 }
-
-const newProjectButton = document.getElementById('newcomposition')
-newProjectButton && newProjectButton.addEventListener("click", (e) => {
-  let newtitle = document.getElementById('newtitle').value
-  const privacyLevel = document.querySelector('input[name="newMusicPrivacyRadios"]:checked').value
-  if (!newtitle) {
-    alert('Introduce a valid title, please')
-    return
-  }
-  let body = JSON.stringify({
-    title: newtitle,
-    privacy_level: privacyLevel
-  })
-
-
-  fetch(ENDPOINT + '/newcomposition', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
-    body: body
-  })
-    .then((r) => {
-      if (!r.ok) {
-        errorIs = r.statusText
-      }
-      return r.json()
-    })
-    .then(data => {
-      if (data.composition) {
-        window.location.href = uriCompositionPage + data.composition.uuid
-      } else {
-        throw new Error(data)
-      }
-    }).catch((error) => {
-      errorIs = error
-    })
-
-})
