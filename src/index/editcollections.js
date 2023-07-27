ecf00a3a7e80ce0526d6ba04be8a31f5a8c916d0
fan-droide <jose.gilpanal@telecom-paris.fr>
2023-07-27 11:02:21 +0200

    update collection title after blur event from input field

--- src/index/editcollections.js
+++ src/index/editcollections.js
@@ -14,16 +14,16 @@ const fetchCollectionsTree = async () => {
 
 const createTreeHTML = (item) => {
     let html = `
-        <span id="removeCollIcon${item.uuid}" data-uuid="${item.uuid}" data-title="${item.title}" role="button" class="badge badge-pill badge-danger" hidden>-</span>
-        <li id="${item.uuid}" class="list-group-item border-bottom-0 border-right-0 border-top-0 border-warning">
-            <input type="text" class="form-control border-secondary" id="treecollectiontitleinput" placeholder="Type a new title"
-            title="collectiontitle" value="${item.title}" disabled>
+        <span id='removeCollIcon${item.uuid}' data-uuid='${item.uuid}' data-title='${item.title}' role='button' class='badge badge-pill badge-danger' hidden>-</span>
+        <li id='${item.uuid}' class='list-group-item border-bottom-0 border-right-0 border-top-0 border-warning'>
+            <input type='text' class='form-control border-secondary' id='treecolltitleinput${item.uuid}' data-uuid='${item.uuid}' placeholder='Type a new title'
+            title='collectiontitle' value='${item.title}' disabled>
         </li>`
     
     if (item.compositions.length > 0 || item.collections.length > 0) {
         html += '<ul>'
         for (const composition of item.compositions) {
-            html += `<li class="list-group-item border-0"><a href='${uriCompositionPage + composition.uuid}'><u>${composition.title}</u></a></li>`
+            html += `<li class='list-group-item border-0'><a href='${uriCompositionPage + composition.uuid}'><u>${composition.title}</u></a></li>`
         }
         for (const collection of item.collections) {
             html += createTreeHTML(collection)
@@ -59,29 +59,30 @@ const clickEditButtonHandler = () => {
 
 const enableEdition = () => {
     
-    const elementsWithHiddenAttribute = document.querySelectorAll('[id*="removeCollIcon"]')
+    const elementsWithHiddenAttribute = document.querySelectorAll(`[id*='removeCollIcon']`)
 
     elementsWithHiddenAttribute.forEach(element => {
         element.removeAttribute('hidden')        
         removeCollectionClickhHandler(element.getAttribute('data-uuid'), element.getAttribute('data-title'))
     })   
     
-    const elementsWithDisabledAttribute = document.querySelectorAll('[id*="treecollectiontitleinput"]')
+    const elementsWithDisabledAttribute = document.querySelectorAll(`[id*='treecolltitleinput']`)
 
     elementsWithDisabledAttribute.forEach(element => {
         element.removeAttribute('disabled')
+        updateInputTextEventHandler(element.getAttribute('data-uuid'), element.value)
     })
 }
 
 const disableEdition = () => {
 
-    const elementsWithHiddenAttribute = document.querySelectorAll('[id*="removeCollIcon"]')
+    const elementsWithHiddenAttribute = document.querySelectorAll(`[id*='removeCollIcon']`)
 
     elementsWithHiddenAttribute.forEach(element => {
         element.setAttribute('hidden', 'true')
     })
 
-    const elementsWithDisabledAttribute = document.querySelectorAll('[id*="treecollectiontitleinput"]')    
+    const elementsWithDisabledAttribute = document.querySelectorAll(`[id*='treecolltitleinput']`)    
 
     elementsWithDisabledAttribute.forEach(element => {
         element.setAttribute('disabled', 'true')
@@ -103,7 +104,7 @@ const confirmDeleteCollectionModal = async (event, collectionId, collectionTitle
 
     if (chk.tagName === 'SPAN') {
 
-        if (confirm(`Do you want remove the collection: ${collectionTitle}?`) == true) {
+        if (confirm(`Are you sure you want remove the collection ${collectionTitle} and all of its content?`) == true) {
 
             const response = await fetch(ENDPOINT + '/deletecollection/' + collectionId, { method: 'DELETE' })
             if (response?.ok) {
@@ -121,4 +122,35 @@ const removeCollectionClickhHandler = (collectionId, collectionTitle) => {
     document.getElementById('removeCollIcon' + collectionId).onclick= async (event) => {
         await confirmDeleteCollectionModal(event, collectionId, collectionTitle)
     }
+}
+
+const updateInputTextEventHandler = (collectionId, currentTitle) => {    
+    document.getElementById('treecolltitleinput'+collectionId).onblur = (event) => {
+        handleInputBlur(event, collectionId, currentTitle)
+    }    
+}
+
+const updateCollectionTitleRqst = (value, uuid) => {
+   
+    const updateTitleApi = ENDPOINT + '/updatecolltitle'
+    const data = { title: value, uuid: uuid }
+    
+    fetch(updateTitleApi, {
+        method: 'PATCH', 
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then()
+    .catch(error => {
+        console.error('Error updating value:', error)
+    })
+}
+
+const handleInputBlur = (event, collId, currTitle) => {    
+    const newValue = event.target.value
+    if(currTitle !== newValue){
+        updateCollectionTitleRqst(newValue, collId)
+    }    
 }
