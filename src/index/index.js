e064e5e3b8ed4868b2351bec16350a55be169ebd
fan-droide <jose.gilpanal@telecom-paris.fr>
2023-09-28 14:29:19 +0200

    refactor index.js, differentiate if user is logged or not to display own compositions or recent

--- src/index/index.js
+++ src/index/index.js
@@ -21,66 +21,90 @@ document.getElementById('useroptions').innerHTML = `<li class='nav-item'>
         <a class='nav-link' href='#' id='openMyCollectionsButton' data-toggle='modal' data-target='#editCollectionsModal'>/ My Collections</a>
     </li>`
 
+const getMyProfile = async (doAfterIfLogged, doAfterIfNotLogged) => {
+  await getJsonApi("/profile", async (r) => {
 
-getJsonApi("/profile", (r) => {
+    const isAuthenticated = ('ok' in r && r['ok'] == true);
+  
+    if (isAuthenticated) {
+      document.getElementById('userlogin').style.display = "none";
+      document.getElementById('useroptions').style.display = "";
+      document.getElementById('display_profile_name').innerText = `[${r.name}]`;
+      await doAfterIfLogged(isAuthenticated)
+    } else {
+      await doAfterIfNotLogged(isAuthenticated)
+    }
+  
+  });
+}
 
-  const isAuthenticated = ("ok" in r && r['ok'] == true);
+const getMyCompositions = async (isAuth) => {
+  await getJsonApi("/mycompositions", (data) => {
 
-  if (isAuthenticated) {
-    document.getElementById('userlogin').style.display = "none";
-    document.getElementById('useroptions').style.display = "";
-    document.getElementById('display_profile_name').innerText = `[${r.name}]`;
-  }
+    if ("compositions" in data)
+      return renderHomePage(data.compositions, "My Compositions", isAuth)
 
-});
+    alert("invalid return value for compisitions list");
 
+  }, (error) => { alert(error) })
+}
+const getRecentCompositions = async (isAuth) => {
+  getJsonApi("/recentcompositions", (data) => {
 
-getJsonApi("/compositions", (data) => {
+    if ("compositions" in data)
+      return renderHomePage(data.compositions, "Last recent compositions", isAuth)
 
-  if ("compositions" in data)
-    return renderHomePage(data.compositions)
+    alert("invalid return value for compisitions list");
 
-  alert("invalid return value for compisitions list");
+  }, (error) => { alert(error) })
+}
 
-}, (error) => {alert(error)})
-
-
-const renderHomePage = (compositionsList, error) => {
+const renderHomePage = (compositionsList, renderOpt, isAuth) => {
 
   const loaderElement = document.getElementById('loader')
   loaderElement.classList.remove('loader')
 
-  if (error) {
-    alert(error)
-  } else {
-    paintListOfCompositions(compositionsList)
-  }
+  paintListOfCompositions(compositionsList, renderOpt, isAuth)
+
 }
 
-const paintListOfCompositions = (compositionsList) => {
-  compositionsList.forEach((element) => {
-    if (element) {
-      let collection = ''
-      if (element.collection) {
-        collection = `<a href='./index.html?collection=${element.collection}' class='card-link green'>${element.collection}</a>`
-      }
-      const template = `
-        <div class='grid-div'>
-          <div class='card'>
-            ${element.opentocontrib ? '<span class="badge badge-info">OPEN TO CONTRIB</span>' : ''}
-            <a href='${uriCompositionPage + element.uuid}'>
-              <img src='${element.photo_url || COMPOSITION_COVER}' alt='Card image cap' class='card-img'>
-            </a>
-            <div class='card-container'>
-              <a href='${uriCompositionPage + element.uuid}' class='card-link blue'>${element.title}</a>
-              <p>${collection}</p>
+const paintListOfCompositions = (compositionsList, renderOpt, isAuth) => {
+  
+  if(!compositionsList.length && isAuth){
+    document.getElementById('initialmessage').hidden = false
+  } else {
+
+    compositionsList.forEach((element) => {
+      if (element) {
+        let collection = ''
+        if (element.collection) {
+          collection = `<a href='./index.html?collection=${element.collection}' class='card-link green'>${element.collection}</a>`
+        }
+        const template = `
+          <div class='grid-div'>
+            <div class='card'>
+              ${element.opentocontrib ? '<span class="badge badge-info">OPEN TO CONTRIB</span>' : ''}
+              <a href='${uriCompositionPage + element.uuid}'>
+                <img src='${element.photo_url || COMPOSITION_COVER}' alt='Card image cap' class='card-img'>
+              </a>
+              <div class='card-container'>
+                <a href='${uriCompositionPage + element.uuid}' class='card-link blue'>${element.title}</a>
+                <p>${collection}</p>
+              </div>
             </div>
           </div>
-        </div>
-      `
-      listElelemts += template
-    }
-  })
-  document.getElementById('grid').insertAdjacentHTML('afterbegin', listElelemts)
-  document.getElementById('searchInput').removeAttribute('disabled')
+        `
+        listElelemts += template
+      }
+    })
+    document.getElementById('grid').insertAdjacentHTML('afterbegin', listElelemts)
+    document.getElementById('searchInput').removeAttribute('disabled')
+    document.getElementById('homepageview').innerHTML = renderOpt
+  }  
 }
+
+const initHomPage = async () => {
+  await getMyProfile(getMyCompositions,getRecentCompositions)  
+}
+
+initHomPage()
