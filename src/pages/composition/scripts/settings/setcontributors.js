ddfc212c0d69889ab61b6ac0f0ce7b4ea4f1b06f
fan-droide <jose.gilpanal@telecom-paris.fr>
2023-12-13 14:50:51 +0100

    fix adding users by email and removing them

--- src/pages/composition/scripts/settings/setcontributors.js
+++ src/pages/composition/scripts/settings/setcontributors.js
@@ -48,7 +48,7 @@ export const addContributorButtonHandler = (compositionId) => {
         document.getElementById('contributorinput').classList.remove('is-invalid')     
         document.getElementById('validationemailresult').innerText = ''        
         if (input.value && looksLikeMail(input.value)) {           
-            addContributorToList(ul, input.value, compositionId, role)
+            addContributorToList(ul, input.value.trim(), compositionId, role)
         } else {
             document.getElementById('contributorinput').classList.add('is-invalid')     
             document.getElementById('validationemailresult').innerText = 'Sorry, invalid email address format.'
@@ -61,14 +61,14 @@ const addContributorToUI = (ul, contrib) => {
     const li = document.createElement('li')
     li.className = 'list-group-item'
     li.id = contrib.user_uid
-    li.textContent = contrib.user_uid + ' (' + role + ')'    
+    li.textContent = contrib.email + ' (' + role + ')'    
     const deleteSwitch = `&nbsp;<div class='custom-control custom-switch custom-control-inline float-right'>
     <input type='checkbox' class='custom-control-input is-invalid' id='removeContSwitch${contrib.user_uid}'>
     <label class='custom-control-label is-invalid' for='removeContSwitch${contrib.user_uid}'>Remove</label>
   </div>`
     li.innerHTML += deleteSwitch
     ul.appendChild(li)
-    removeContributorSwitchHandler(contrib.user_uid)
+    removeContributorSwitchHandler(contrib)
 }
 
 const addContributorToList = async (ul, contrib, compositionId, role) => {
@@ -76,8 +76,8 @@ const addContributorToList = async (ul, contrib, compositionId, role) => {
     if(parseInt(roleInput) !== 0){
         
         const newcontrib = { email:contrib, user_uid: contrib, composition_id: compositionId, role: parseInt(role) }        
-        const indexContribDuplicateInCurrent = CURRENT_CONTRIBUTORS.findIndex(x => x.user_uid === contrib)
-        const indexContribDuplicateInNew = NEW_CONTRIBUTORS.findIndex(x => x.user_uid === contrib)
+        const indexContribDuplicateInCurrent = CURRENT_CONTRIBUTORS.findIndex(x => x.email === contrib)
+        const indexContribDuplicateInNew = NEW_CONTRIBUTORS.findIndex(x => x.email === contrib)
 
         let canAdd = false
 
@@ -90,7 +90,11 @@ const addContributorToList = async (ul, contrib, compositionId, role) => {
         if(canAdd){
             // #TODO: replace with API call to new endpoint to validate contributor
             const response = await fetch(ENDPOINT + '/checkuser/' + contrib)                       
-            if(response?.ok || (response?.status === 404)){                                        
+            if(response?.ok || (response?.status === 404)){
+                if(response?.status === 200){
+                    const respJson = await response.json()
+                    newcontrib.user_uid = respJson.user_uid
+                }                                                        
                 NEW_CONTRIBUTORS.push(newcontrib)            
                 addContributorToUI(ul, newcontrib)
                 if(response?.status === 404){
@@ -99,7 +103,9 @@ const addContributorToList = async (ul, contrib, compositionId, role) => {
             } else {
                 alert(`User can't be added`)
             }
-        }        
+        } else {
+            alert('Duplicate user')
+        }       
     }   
 }
 
@@ -148,8 +154,12 @@ export const saveNewContributors = async () => {
         for (let i=0; i < NEW_CONTRIBUTORS.length; i++){                    
             const newcontrib = NEW_CONTRIBUTORS[i]
             const resultAddContrib = await updateSettings('POST', '/addcontributorbyemail', newcontrib)                    
-            if(resultAddContrib?.ok){                        
-                NEW_CONTRIBUTORS[i].id = resultAddContrib.contribid
+            if(resultAddContrib?.ok){                
+                NEW_CONTRIBUTORS[i].id = resultAddContrib.contribid                
+                if(newcontrib.email === newcontrib.user_uid){
+                    NEW_CONTRIBUTORS[i].user_uid = resultAddContrib.uuid
+                    document.getElementById(newcontrib.email).id = resultAddContrib.uuid                    
+                }                
                 copy_new_contribs[i] = null                       
             }                       
         }
@@ -164,16 +174,16 @@ export const saveRemoveContributors = async (compId) => {
     if(TOREMOVE_CONTRIBUTORS.length > 0){
         let copy_toremove_contribs = [...TOREMOVE_CONTRIBUTORS]
         for (let j=0; j < TOREMOVE_CONTRIBUTORS.length; j++){ 
-            const indexContribInCurrent = CURRENT_CONTRIBUTORS.findIndex(x => x.user_uid === TOREMOVE_CONTRIBUTORS[j])                    
+            const indexContribInCurrent = CURRENT_CONTRIBUTORS.findIndex(x => x.email === TOREMOVE_CONTRIBUTORS[j])                              
             const contribToRemId = CURRENT_CONTRIBUTORS[indexContribInCurrent].user_uid 
             const contribToRem = {contrib_uuid:contribToRemId, comp_uuid:compId}
             const resultRemoveContrib = await updateSettings('DELETE', '/deletecontributor', contribToRem)                    
             if(resultRemoveContrib?.ok){                        
                 copy_toremove_contribs[j] = null
                 CURRENT_CONTRIBUTORS.splice(indexContribInCurrent,1)
-                document.getElementById(TOREMOVE_CONTRIBUTORS[j]).remove()
+                document.getElementById(contribToRemId).remove()
             } else {
-                document.getElementById('removeContSwitch'+TOREMOVE_CONTRIBUTORS[j]).checked = false
+                document.getElementById('removeContSwitch'+contribToRemId).checked = false
             }                   
         }
         const noEmptyValuesRm = copy_toremove_contribs.filter((value) => value != null)
@@ -181,19 +191,19 @@ export const saveRemoveContributors = async (compId) => {
     }         
 }
 
-const removeContributorSwitchHandler = (contribId) => {
+const removeContributorSwitchHandler = (contrib) => {
     
-    document.getElementById('removeContSwitch'+contribId).addEventListener('change', function(event) {        
+    document.getElementById('removeContSwitch'+contrib.user_uid).addEventListener('change', function(event) {        
         const chk = event.target        
         if (chk.tagName === 'INPUT' && chk.type === 'checkbox') {        
             if(chk.checked){                
-                if (confirm(`Do you want remove the contributor: ${contribId}?`) == true) {                    
-                    const indexContribInNew = NEW_CONTRIBUTORS.findIndex(x => x.user_uid === contribId)                    
+                if (confirm(`Do you want remove the contributor: ${contrib.email}?`) == true) {                    
+                    const indexContribInNew = NEW_CONTRIBUTORS.findIndex(x => x.email === contrib.email)                                     
                     if(indexContribInNew > -1){                        
                         NEW_CONTRIBUTORS.splice(indexContribInNew,1)
-                        document.getElementById(contribId).remove()
+                        document.getElementById(contrib.user_uid).remove()
                     } else {                        
-                        TOREMOVE_CONTRIBUTORS.push(contribId)
+                        TOREMOVE_CONTRIBUTORS.push(contrib.email)
                     }                 
                 } else {                   
                     event.target.checked = false
