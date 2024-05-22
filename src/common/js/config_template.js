485e43a394d4a55a9cf307220d4a628231313f79
fan-droide <jose.gilpanal@telecom-paris.fr>
2024-05-22 16:15:36 +0200

    use window.location.host instead of localhost

--- src/common/js/config_template.js
+++ src/common/js/config_template.js
@@ -1,18 +1,17 @@
 const MODE = 'DEV' // 'STAGE'
-const DEVPORT = 7007
 
 const ENVIRONMENTS = {
     PROD:{       
         ENDPOINT:'',
-        UPLOAD_ENDPOINT:'',      
+        UPLOAD_ENDPOINT:''     
     },
     STAGE:{        
         ENDPOINT: window.location.protocol+'//'+window.location.host,
         UPLOAD_ENDPOINT: window.location.protocol+'//'+window.location.host+'/fileUpload'
     },
     DEV:{       
-        ENDPOINT:'https://localhost:'+DEVPORT,
-        UPLOAD_ENDPOINT:'https://localhost:'+DEVPORT+'/fileUpload'         
+        ENDPOINT:window.location.protocol+'//'+window.location.host,
+        UPLOAD_ENDPOINT:window.location.protocol+'//'+window.location.host + '/fileUpload'
     }
 }
 
