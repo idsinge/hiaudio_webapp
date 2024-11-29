ff86dbf5dfeb98066df093e9220d6224521d7bbe
fan-droide <jose.gilpanal@telecom-paris.fr>
2024-11-29 16:41:16 +0100

    new metronome version with accent using worker

--- src/pages/composition/scripts/metronome/metronome.js
+++ src/pages/composition/scripts/metronome/metronome.js
@@ -1,108 +1,107 @@
-import { tempo, noteResolution } from './metronomehandler'
+export default class Metronome {
 
-export let audioContext = null;
-var unlocked = false;
-export let isPlaying = false;      // Are we currently playing?
-//var startTime;              // The start time of the entire sequence.
-var current16thNote;        // What note is currently last scheduled?
-//export let tempo = 80.0;          // tempo (in beats per minute)
-var lookahead = 25.0;       // How frequently to call scheduling function 
-//(in milliseconds)
-var scheduleAheadTime = 0.1;    // How far ahead to schedule audio (sec)
-// This is calculated from lookahead, and overlaps 
-// with next interval (in case the timer is late)
-var nextNoteTime = 0.0;     // when the next note is due.
-// var noteResolution = 0;     // 0 == 16th, 1 == 8th, 2 == quarter note
-var noteLength = 0.05;      // length of "beep" (in seconds)
-var notesInQueue = [];      // the notes that have been put into the web audio,
-// and may or may not have played yet. {note, time}
-var timerWorker = null;     // The Web Worker used to fire timer messages
+    audioContext = null
+    notesInQueue = []
+    currentQuarterNote = 0    
+    tempo = 120
+    lookahead = 25          // How frequently to call scheduling function (in milliseconds)
+    scheduleAheadTime = 0.1   // How far ahead to schedule audio (sec)
+    nextNoteTime = 0.0     // when the next note is due
+    isRunning = false
+    timerWorker = null
+    swing = 0 // a value for 'swing' between zero and one hundred
+    accent = 4
+    barLength = 8
+    currentEighthNote = 0
+    activate = false
 
-
-function nextNote() {
-    // Advance current note and time by a 16th note...
-    var secondsPerBeat = 60.0 / tempo;    // Notice this picks up the CURRENT 
-    // tempo value to calculate beat length.
-    nextNoteTime += 0.25 * secondsPerBeat;    // Add beat length to last beat time
-
-    current16thNote++;    // Advance the beat number, wrap to zero
-    if (current16thNote == 16) {
-        current16thNote = 0;
-    }
-}
-
-function scheduleNote(beatNumber, time) {
-    // push the note on the queue, even if we're not playing.
-    notesInQueue.push({ note: beatNumber, time: time });
-
-    if ((noteResolution == 1) && (beatNumber % 2))
-        return; // we're not playing non-8th 16th notes
-    if ((noteResolution == 2) && (beatNumber % 4))
-        return; // we're not playing non-quarter 8th notes
-
-    // create an oscillator
-    var osc = audioContext.createOscillator();
-    osc.connect(audioContext.destination);
-    if (beatNumber % 16 === 0)    // beat 0 == high pitch
-        osc.frequency.value = 880.0;
-    else if (beatNumber % 4 === 0)    // quarter notes = medium pitch
-        osc.frequency.value = 440.0;
-    else                        // other 16th notes = low pitch
-        osc.frequency.value = 220.0;
-
-    osc.start(time);
-    osc.stop(time + noteLength);
-}
-
-function scheduler() {
-    // while there are notes that will need to play before the next interval, 
-    // schedule them and advance the pointer.
-    while (nextNoteTime < audioContext.currentTime + scheduleAheadTime) {
-        scheduleNote(current16thNote, nextNoteTime);
-        nextNote();
-    }
-}
-
-export function playMetronome() {
-    if (!audioContext)
-        audioContext = new AudioContext();
-
-    if (!unlocked) {
-        // play silent buffer to unlock the audio
-        var buffer = audioContext.createBuffer(1, 1, 22050);
-        var node = audioContext.createBufferSource();
-        node.buffer = buffer;
-        node.start(0);
-        unlocked = true;
-    }
-
-    isPlaying = !isPlaying;
-
-    if (isPlaying) { // start playing
-        current16thNote = 0;
-        nextNoteTime = audioContext.currentTime;
-        timerWorker.postMessage("start");
-        return "stop";
-    } else {
-        timerWorker.postMessage("stop");
-        return "play";
-    }
-}
-
-export function initMetronome() {
-
-    timerWorker = new Worker(
-        new URL('metronomeworker.js', import.meta.url),
-        { type: 'module' }
-    )
-
-    timerWorker.onmessage = function (e) {
-        if (e.data == "tick") {
-            // console.log("tick!");
-            scheduler();
+    init() {
+        this.timerWorker = new Worker(
+            new URL('metronomeworker.js', import.meta.url),
+            { type: 'module' }
+        )
+        this.timerWorker.onmessage = (e) => {
+            if (e.data === 'tick') {
+                this.scheduler()
+            }
         }
-        else
-            console.log("message: " + e.data);
-    };
-    timerWorker.postMessage({ "interval": lookahead });
+        this.timerWorker.postMessage({ 'interval': this.lookahead })
+    }
+
+    nextNote() {
+        const sw = this.swing / 100
+        // Advance current note and time by a quarter note (crotchet if you're posh)
+        const secondsPerBeat = 60.0 / this.tempo // Notice this picks up the CURRENT tempo value to calculate beat length.     
+        if (this.currentEighthNote == 0) {
+            this.nextNoteTime += secondsPerBeat * sw
+        }
+        else {
+            this.nextNoteTime += secondsPerBeat * (1 - sw)
+        }
+        this.currentQuarterNote++    // Advance the beat number, wrap to zero
+        if (this.currentQuarterNote == (this.barLength)) {
+            this.currentQuarterNote = 0
+        }
+        if (this.currentEighthNote == 0) {
+            this.currentEighthNote = 1
+        }
+        else { this.currentEighthNote = 0 }
+    }
+
+    scheduleNote(beatNumber, time) {
+        // push the note on the queue, even if we're not playing.
+        this.notesInQueue.push({ note: beatNumber, time: time })        
+        const osc = this.audioContext.createOscillator()
+        const envelope = this.audioContext.createGain()
+        if (this.currentQuarterNote == 0) {
+            osc.frequency.value = 2000
+        }
+        else {
+            osc.frequency.value = (beatNumber % this.accent == 0) ? 1000 : 800
+        }
+        envelope.gain.value = 1
+        envelope.gain.exponentialRampToValueAtTime(1, time + 0.001)
+        envelope.gain.exponentialRampToValueAtTime(0.001, time + 0.02)
+        osc.connect(envelope)
+        envelope.connect(this.audioContext.destination)
+        osc.start(time)
+        osc.stop(time + 0.03)
+    }
+
+    scheduler() {
+        // while there are notes that will need to play before the next interval, schedule them and advance the pointer.
+        while (this.nextNoteTime < this.audioContext.currentTime + this.scheduleAheadTime) {
+            this.scheduleNote(this.currentQuarterNote, this.nextNoteTime)
+            this.nextNote()
+        }
+    }
+
+    start() {
+        if (this.isRunning) return
+
+        if (this.audioContext == null) {
+            this.audioContext = new (window.AudioContext || window.webkitAudioContext)()
+        }
+
+        this.isRunning = true
+
+        this.currentQuarterNote = 0
+        this.nextNoteTime = this.audioContext.currentTime + 0.05
+
+        this.timerWorker.postMessage('start')
+    }
+
+    stop() {
+        this.isRunning = false
+        this.timerWorker.postMessage('stop')
+    }
+
+    startStop() {
+        if (this.isRunning) {
+            this.stop()
+        }
+        else {
+            this.start()
+        }
+    }
 }
--- src/pages/composition/scripts/metronome/metronomehandler.js
+++ src/pages/composition/scripts/metronome/metronomehandler.js
@@ -1,64 +1,177 @@
 import DynamicModal from '../../../../common/js/modaldialog'
-import { initMetronome, playMetronome, audioContext, isPlaying } from './metronome'
+import Metronome from './metronome'
 
-
-export let tempo = 80.0;  // tempo (in beats per minute)
-export let noteResolution = 0;     // 0 == 16th, 1 == 8th, 2 == quarter note
+export let metronome = null
 
 const metronomeFinishCallback = () => {
-    console.log('finish callback')
+    if(metronome?.isRunning){
+        metronome.stop()
+    }
 }
 
-const tempoSliderHandler = (event) => {
-    tempo = event.target.value
-    document.getElementById('showTempo').innerText = event.target.value
+const tempoChangeHandler = () => {
+    const tempo = document.getElementById('tempo')
+    tempo.textContent = metronome.tempo
+    const tempoChangeButtons = document.querySelectorAll(`[data-id='tempo-change']`)
+    for (let i = 0; i < tempoChangeButtons.length; i++) {
+        tempoChangeButtons[i].addEventListener('click', function() {
+            const val_add = parseInt(this.dataset.change)
+            const valid_num = metronome.tempo + val_add
+            if(valid_num >= 0){
+                metronome.tempo = valid_num
+                tempo.textContent = metronome.tempo
+            }
+        })
+    }
 }
+
+const accentChangeHandler = () => {
+    const accent = document.getElementById('accent')
+    accent.textContent = metronome.accent
+    const accentChangeButtons = document.querySelectorAll(`[data-id='accent-change']`)
+    for (let i = 0; i < accentChangeButtons.length; i++) {
+        accentChangeButtons[i].addEventListener('click', function() {
+            const val_add = parseInt(this.dataset.change)
+            const valid_num = metronome.accent + val_add
+            if(valid_num >= 0){
+                metronome.accent = valid_num
+                accent.textContent = metronome.accent
+            }
+        })
+    }
+}
+
+const barChangeHandler = () => {
+    const bar = document.getElementById('bar')
+    bar.textContent = metronome.barLength
+    const barChangeButtons = document.querySelectorAll(`[data-id='bar-change']`)
+    for (let i = 0; i < barChangeButtons.length; i++) {
+        barChangeButtons[i].addEventListener('click', function() {
+            const val_add = parseInt(this.dataset.change)
+            const valid_num = metronome.barLength + val_add
+            if(valid_num >= 0){
+                metronome.barLength = valid_num
+                bar.textContent = metronome.barLength
+            }
+        })
+    }
+}
+
+const swingChangeHandler = () => {
+    const swing = document.getElementById('swing')
+    swing.textContent = metronome.swing
+    const swingChangeButtons = document.querySelectorAll(`[data-id='swing-change']`)
+    for (let i = 0; i < swingChangeButtons.length; i++) {
+        swingChangeButtons[i].addEventListener('click', function() {
+            const val_add = parseInt(this.dataset.change)
+            const valid_num = metronome.swing + val_add
+            if(valid_num >= 0){
+                metronome.swing = valid_num
+                swing.textContent = metronome.swing
+            }
+        })
+    }
+}
+
 
 const playButtonHandler = () => {
-    document.getElementById('play-metronome-btn').innerText = playMetronome()
+    
+    const playPauseIcon = document.getElementById('play-pause-icon')
+    metronome.startStop()
+    if (metronome.isRunning) {
+        playPauseIcon.className = 'pause-metronome-icon'
+    }
+    else {
+        playPauseIcon.className = 'play-metronome-icon'
+    }
 }
 
-const noteResolutionHandler = (event) => {
-    noteResolution = event.target.selectedIndex
+const activationHandler = () => {
+    metronome.activate = !metronome.activate
 }
 
 const openMetronomeDialog = () => {
     DynamicModal.dynamicModalDialog(
-        `<div id="controls">
-            <div>
-                <button id="play-metronome-btn" type="button" class="btn btn-primary btn-lg">play</button>
-            </div><br>
-            <div id="tempoBox">
-                Tempo: <span id="showTempo">80</span>BPM 
-                <input id="tempo-slider" type="range" min="30.0" max="160.0" step="1" value="80" style="height: 20px; width: 200px">
+        `<div class="metronome-controls">
+            <div class="metronome-switch custom-control custom-switch">
+                <input type="checkbox" class="custom-control-input" id="metronomeSwitch">
+                <label class="custom-control-label" for="metronomeSwitch">Activate</label>
             </div>
-            <div>
-                Resolution:
-                <select id="note-res-select">
-                    <option>16th notes
-                    <option>8th notes
-                    <option>Quarter notes
-                </select>
+        </div>
+        <div class="metronome-controls">
+            <button id="play-button" class="metronome-button play-pause-metronome-button btn-success">
+                <div id="play-pause-icon" class="play-metronome-icon"></div>
+            </button>
+        </div>        
+        <div class="metronome-controls">
+        
+            <button data-id="tempo-change" class="metronome-button" data-change="-5">-5</button>
+            <button data-id="tempo-change" class="metronome-button" data-change="-1">-</button>
+
+            <div class="tempo-container">
+                <div id="tempo" class="number-metronome">120</div>
+                <div class="subtext-metronome">bpm</div>
             </div>
+
+            <button data-id="tempo-change" class="metronome-button" data-change="+1">+</button>
+            <button data-id="tempo-change" class="metronome-button" data-change="+5">+5</button>
+        </div>    
+
+        <div class="metronome-controls">
+            <button data-id="accent-change" class="metronome-button" data-change="-1">-</button>
+            <div class="bar-container">
+                <div id="accent" class="number-metronome">4</div>
+                <div class="subtext-metronome">accent</div>
+            </div>
+            <button data-id="accent-change" class="metronome-button" data-change="+1">+</button>
+        </div>
+
+        <div class="metronome-controls">
+            <button data-id="bar-change" class="metronome-button" data-change="-1">-</button>
+            <div class="tempo-container">
+                <div id="bar" class="number-metronome">0</div>
+                <div class="subtext-metronome">1/8 in bar</div>
+            </div>
+            <button data-id="bar-change" class="metronome-button" data-change="+1">+</button>
+        </div>
+
+        <div class="metronome-controls">
+            <button data-id="swing-change" class="metronome-button" data-change="-5">-5</button>
+            <button data-id="swing-change" class="metronome-button" data-change="-1">-</button>
+
+            <div class="tempo-container">
+                <div id="swing" class="number-metronome">0</div>
+                <div class="subtext-metronome">swing</div>
+            </div>
+
+            <button data-id="swing-change" class="metronome-button" data-change="+1">+</button>
+            <button data-id="swing-change" class="metronome-button" data-change="+5">+5</button>
         </div>
        `,
         null,
         '',
         'Close',
         'Metronome',
-        'bg-success',
+        'bg-secondary',
         metronomeFinishCallback
     )
-    if (!audioContext) {
-        initMetronome()
+    if (!metronome) {
+        metronome = new Metronome()
+        metronome.init()
     }
-    if (isPlaying) {
-        document.getElementById('play-metronome-btn').innerText = 'stop'
+    if (metronome.isRunning) {
+        document.getElementById('play-pause-icon').className = 'pause-metronome-icon'
     }
-    document.getElementById('play-metronome-btn').onclick = playButtonHandler
-    document.getElementById('tempo-slider').oninput = tempoSliderHandler
-    document.getElementById('note-res-select').onchange = noteResolutionHandler
-
+    
+    const metronomeSwitch = document.getElementById('metronomeSwitch')    
+    metronomeSwitch.checked = metronome.activate
+    
+    document.getElementById('play-button').onclick = playButtonHandler
+    tempoChangeHandler()
+    accentChangeHandler()
+    barChangeHandler()
+    swingChangeHandler()
+    metronomeSwitch.onclick = activationHandler
 }
 
 export const triggerMetronomeButton = () => {
